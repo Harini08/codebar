bdc6714e0f96c9298afce6d6e6d530e33ae849fb
Harini Manivannan <harini.manivannan@gmail.com>
2014-03-26 20:09:02 +0000

    Modifying DOM HTML for London Variable
    with link on html

--- lesson 8 - Beginning JS/london-script.js
+++ lesson 8 - Beginning JS/london-script.js
@@ -1,5 +1,25 @@
 // used in london.html
 
+var london = {
+  name: "London",
+  population: 8308369,
+  tallestBuilding: {
+     name:  "Shard",
+     height: "310m"
+  },
+  numberOfUniversities: 43,
+  averageRent: 1106,
+  dailyTubePassengerJourney: 3500000,
+  olympics: [ 1908, 1948, 2012]
+}
+
+function displayPopulation() {
+  var population = document.createElement("p");
+  var content = document.createTextNode("Population: " + london.population);
+  population.appendChild(content);
+  document.body.appendChild(population);
+}
+
 function listDomElements() {
   var children = document.body.childNodes;
   for(var i=0; i <  children.length; i++) {
