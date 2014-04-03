e644e4bac113af526f9f0f8079d2a277fd657553
Harini Manivannan <harini.manivannan@gmail.com>
2014-04-03 18:59:59 +0100

    lesson 9
    extra bits

--- lesson 9 - Intro to JQuery/script.js
+++ lesson 9 - Intro to JQuery/script.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
  $(document).on('click', 'button', function(){
   var itemToAdd = $("#item").val();
   addToList(itemToAdd);
+  $("#item").val("");
   });
 
   function addToList(item){
     $("ol#items").append("<li>" + item + "</li>")
   };
   addToList("build a website");
-
 });
