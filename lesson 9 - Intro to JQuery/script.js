706a1593f26f688dadfcc6516dc8d3fb253230b6
Harini Manivannan <harini.manivannan@gmail.com>
2014-04-09 20:40:49 +0100

    Exercise 1: Build a wish list
    added label items and setting items to complete

--- lesson 9 - Intro to JQuery/script.js
+++ lesson 9 - Intro to JQuery/script.js
@@ -5,11 +5,18 @@ $(document).ready(function(){
  $(document).on('click', 'button', function(){
   var itemToAdd = $("#item").val();
   addToList(itemToAdd);
-  $("#item").val("");
+  $("#item").val("").focus();
   });
 
   function addToList(item){
-    $("ol#items").append("<li>" + item + "</li>")
+    $("ol#items").append("<li>" + item + "<span class='label pending'>Pending</span>" + "</li>")
   };
   addToList("build a website");
 });
+
+  $(document).on('click','.label', function(){
+    var parent_node = $(this).parent();
+    parent_node.addClass(".complete").append("<span class='label success'>Done!</span>");
+    $(this).remove();
+  })
+
